04d5f5a9283d344dab1b91ed5a8eb4dd186e33de
izaacsilva <izcss21@gmail.com>
2018-11-19 19:43:25 -0200

    Alteraões do Azize para o funcionamento dos botões de letras da forca.

--- view/js/script.js
+++ view/js/script.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
-	$('#teclado > button').on('click', function() {
-		$('#tentativa')
+const word = ["fat", "lazy", "hungry"];
+
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum];
+let rightWord = [];
+let wrongWord = [];
+let underScore = [];
+
+var botao = document.getElementsByTagName("button");
+
+
+console.log(choosenWord);
+
+let generateUnderscore = () => {
+    for(let i = 0; i < choosenWord.length; i++){
+        underScore.push('_');        
+    }
+    return underScore;
+}
+
+console.log(generateUnderscore());
+
+function press(){
+    
+    $(document).ready(function() {
+        $(".wordbutton button").click(function() {
+            var indexLetter = $(this).index();  
+            console.log(botao[indexLetter].innerText);      
+    })});
+    
+    
+
+};
+   
+ 
+
